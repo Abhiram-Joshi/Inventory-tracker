815a7713f6026090eb46f4f47b490f8d17ee619f
Abhiram-Joshi <abramjoshi@gmail.com>
2022-01-14 12:13:57 +0530

    feat: add item to inventory

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from rest_framework.generics import CreateAPIView
+from .serializers import ItemSerializer
 # Create your views here.
+
+class CreateItemView(CreateAPIView):
+    serializer_class = ItemSerializer
